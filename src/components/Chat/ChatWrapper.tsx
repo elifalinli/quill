f77b8004cc46117c8cf71e494ccf742abc899667
Elif Alinli <semraelifalinli@gmail.com>
2024-01-03 16:15:04 +0000

    add sending message functionality

--- src/components/Chat/ChatWrapper.tsx
+++ src/components/Chat/ChatWrapper.tsx
@@ -3,12 +3,14 @@
 import { trpc } from "@/app/_trpc/client"
 import ChatInput from "./ChatInput"
 import Messages from "./Messages"
-import { Loader2, XCircle } from "lucide-react"
+import { ChevronLeft, Link, Loader2, XCircle } from "lucide-react"
+import { buttonVariants } from "../ui/button"
 
 interface ChatWrapperProps {
   fileId: string
 }
 const ChatWrapper = ({fileId}: ChatWrapperProps) => {
+
 const {data, isLoading} = trpc.getFileUploadStatus.useQuery(
   {
   fileId,
@@ -31,38 +33,45 @@ if(isLoading)
     <ChatInput isDisabled/>
   </div>
 )
-  if(data?.status === 'PROCESSING') return (
+  if(data?.status === 'PROCESSING') 
+  return (
+   
     <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
     <div className="flex-1 flex justify-center items-center flex-col mb-28">
       <div className="flex flex-col items-center gap-2">
         <Loader2 className="h-8 w-8 text-blue-500 animate-spin"/>
         <h3 className="font-semibold text-xl">Processing PDF...</h3>
-        <p className="text-zinc-500 text-sm"> This wo&apos;nt take long.</p>
+        <p className="text-zinc-500 text-sm"> This won&apos;t take long.</p>
       </div>
     </div>
     <ChatInput isDisabled/>
   </div>
   )
   
-  if(data?.status === 'FAILED') return (
+  if(data?.status === 'FAILED' ) return (
     <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
     <div className="flex-1 flex justify-center items-center flex-col mb-28">
       <div className="flex flex-col items-center gap-2">
         <XCircle className="h-8 w-8 text-red-500"/>
         <h3 className="font-semibold text-xl">Too many pages in PDF</h3>
-        <p className="text-zinc-500 text-sm">Your <span className='font-medium'>Free</span> plan supports up to 7 pages per PDF</p>
+        <p className="text-zinc-500 text-sm">Your <span className='font-medium'>Free</span> plan supports up to 7 pages per PDF
+        </p>
+        <Link href="/dashboard" className={buttonVariants({
+            variant: "secondary",
+            className: "mt-4"
+          })}>
+            <ChevronLeft className="h-3 w-3 mr-1.5"/>Back</Link>
       </div>
     </div>
     <ChatInput isDisabled/>
   </div>
   )
-
+  if(data?.status === 'SUCCESS' )
   return (
     <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
      <div className="flex-1 justify-between flex flex-col mb-28">
       <Messages/>
      </div>
-
      <ChatInput/>
     </div>
   )
